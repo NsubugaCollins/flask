d9c578d7c0b4628ad32118a86d56966a3294f242
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-04-03 14:37:35 +0300

    Fix Railway MySQL connection

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ db = mysql.connector.connect(
     host=os.getenv("MYSQLHOST"),
     user=os.getenv("MYSQLUSER"),
     password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE")
+    database=os.getenv("MYSQLDATABASE"),
+    port=int(os.getenv("MYSQLPORT") or 3306)
 )
 
 cursor = db.cursor()
