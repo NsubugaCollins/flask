1f204d6b3d96ca50257760563ddf110a49885936
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-04-03 15:36:17 +0300

    connect railway mysql variables

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
+
 # Railway environment variables
 db = mysql.connector.connect(
     host=os.getenv("MYSQLHOST"),
