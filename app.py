c2f0554775cf292d3cd07f6fcff64669c802062b
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-04-03 13:21:45 +0300

    Initial Flask commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+# Railway environment variables
+db = mysql.connector.connect(
+    host=os.getenv("MYSQLHOST") or "localhost",
+    user=os.getenv("MYSQLUSER") or "root",
+    password=os.getenv("MYSQLPASSWORD") or "collins123",
+    database=os.getenv("MYSQLDATABASE") or "taskdb",
+    port=int(os.getenv("MYSQLPORT") or 3306)
+)
+
+cursor = db.cursor()
+
+# Home (Read)
+@app.route('/')
+def index():
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+    return render_template('index.html', tasks=tasks)
+
+# Add Task (Create)
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        cursor.execute("INSERT INTO tasks (title) VALUES (%s)", (title,))
+        db.commit()
+        return redirect('/')
+    return render_template('add.html')
+
+# Delete Task
+@app.route('/delete/<int:id>')
+def delete(id):
+    cursor.execute("DELETE FROM tasks WHERE id=%s", (id,))
+    db.commit()
+    return redirect('/')
+
+# Edit Task
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    if request.method == 'POST':
+        title = request.form['title']
+        cursor.execute("UPDATE tasks SET title=%s WHERE id=%s", (title, id))
+        db.commit()
+        return redirect('/')
+    
+    cursor.execute("SELECT * FROM tasks WHERE id=%s", (id,))
+    task = cursor.fetchone()
+    return render_template('edit.html', task=task)
+
+if __name__ == '__main__':
+    app.run(debug=True)
